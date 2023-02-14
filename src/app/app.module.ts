b86a9f268da4df1dd1649537dbc5e7c5bcc2a387
Younes <younesnikbin@gmail.com>
2023-02-14 23:22:16 +0330

    add navbar to home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,39 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './core/home/home.component';
-import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HomeComponent} from './core/home/home.component';
+import {PageNotFoundComponent} from './core/page-not-found/page-not-found.component';
 import {MatButtonModule} from "@angular/material/button";
+import {NavbarComponent} from './core/navbar/navbar.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {faUserCircle} from "@fortawesome/free-solid-svg-icons";
+import {MatTooltipModule} from "@angular/material/tooltip";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    NavbarComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        MatButtonModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    FontAwesomeModule,
+    MatTooltipModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faUserCircle)
+  }
 }
