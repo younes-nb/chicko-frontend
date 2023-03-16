2f996a2e4dfee88228c43cd03e5eda16a93b76f3
Younes <younesnikbin@gmail.com>
2023-03-16 10:23:46 +0330

    chat frame added with animations

--- code/src/app/support/support.component.ts
+++ code/src/app/support/support.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-support',
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        bottom: '15%',
+        opacity: 1,
+      })),
+      state('closed', style({
+        bottom: '7%',
+        opacity: 0,
+      })),
+      transition('open => closed', [
+        animate('0.5s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+    ]),
+  ],
   templateUrl: './support.component.html',
   styleUrls: ['./support.component.scss']
 })
 export class SupportComponent {
+  isChatShowing: boolean = false;
 
+  toggleChat() {
+    this.isChatShowing = !this.isChatShowing;
+  }
 }
